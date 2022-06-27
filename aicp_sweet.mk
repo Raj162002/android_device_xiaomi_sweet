75494a794c7dfce2643bcc3eb9a25c5d155bda46
Rajeevan <81076257+Raj162002@users.noreply.github.com>
2022-06-27 22:54:35 +0530

    bring up

--- aicp_sweet.mk
+++ aicp_sweet.mk
@@ -6,9 +6,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
 # Inherit common ArrowOS configurations
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_sweet
+PRODUCT_NAME := aicp_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10 Pro
