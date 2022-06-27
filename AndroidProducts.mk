b4ba07912b30d54a8f46f87e80cd9058ce36d7e9
Rajeevan <81076257+Raj162002@users.noreply.github.com>
2022-06-27 22:48:48 +0530

    bring up

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_sweet.mk
+    $(LOCAL_DIR)/aicp_sweet.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_sweet-eng \
-    lineage_sweet-userdebug \
-    lineage_sweet-user
+    aicp_sweet-eng \
+    aicp_sweet-userdebug \
+    aicp_sweet-user
